f832a892bb324996229935ad66cf957a885b50c9
Alex Estrada <87288871+stradaa@users.noreply.github.com>
2024-02-18 13:08:31 -0500

    first commit

---
+++ setDefaultUI.m
@@ -0,0 +1,26 @@
+function app = setDefaultUI(app)
+%SETDEFAULTUI Sets initial view of the app
+% duh
+
+%% Video Analysis
+% Enable / Disable
+app.PlayButton.Enable = 'off';
+app.StartSegmentButton.Enable = 'off';
+app.selectButton.Enable = 'off';
+app.selectButton_2.Enable = 'off';
+app.selectButton_3.Enable = 'off';
+app.selectButton_4.Enable = 'off';
+app.UIAxes.Visible = 'off';
+app.Slider.Enable = 'off';
+app.SetButton.Enable = 'off';
+
+% Defaults
+app.FPSEditField.Value = 0;
+app.Label.Text = pwd;
+app.DropDown.Value = '.mat';
+app.EditField.Value = 'Reach';
+app.EditField2.Value = 'Still';
+app.EditField3.Value = 'MIA';
+app.EditField4.Value = 'Other';
+end
+
---
+++ setProperties.m
@@ -0,0 +1,36 @@
+function [D, S] = setProperties(D, S)
+%SETPROPERTIES Sets defautlt properties
+%
+% D - Data
+% S - Status
+%
+
+%% Data
+beh = struct( ...       
+            'vid', [], ...  % row 1 = frame, row 2 = currentTime of frame
+            'duration', 0, ...
+            'default_FPS', 0, ...
+            'labels', table(), ...
+            't_rec_dlc', []);
+
+eeg = struct( ...       % will expand on this later
+            't_rec_t', []);
+
+% set
+D.beh = beh;
+D.eeg = eeg;
+
+
+%% Status
+video_analysis = struct( ...
+                        'base_dir', pwd, ...
+                        'idx', 0, ...
+                        'max_idx', 0, ...
+                        'play', false, ...
+                        'fps', 10, ...
+                        'start_segment', 0, ...
+                        'end_segment', 0);
+% set
+S.video_analysis = video_analysis;
+end
+
